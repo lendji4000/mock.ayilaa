94b008807adfccb96bbcdc5bc8ba9d52998a8fdb
Lenine Djouatsa <lenine.djouatsa@gmail.com>
2023-05-14 11:54:54 +0200

    End of products and services

--- mock/attractions/attractionServices.js
+++ mock/attractions/attractionServices.js
@@ -1,25 +1,30 @@
 // La table attractionServices stocke les informations sur les services proposés dans chaque attraction touristique,
 // telles que le nom, la description et le prix, en référençant la table Locations
-module.exports=[
-    {
-        id: 1,
-        attraction: {},
-        name : "",
-        description: "",
-        price:"",
-    },
-    {
-        id: 2,
-        attraction: {},
-        name : "",
-        description: "",
-        price:"",
-    },
-    {
-        id: 3,
-        attraction: {},
-        name : "",
-        description: "",
-        price:"",
-    },
-]
+
+const { generateRandomWord, generateRandomImageLinks } = require('../globalFunctions');
+const hoaxer = require('hoaxer');
+
+function generateServices () {
+    const services = []
+    for (let id = 1; id <= 500; id += 1) {
+        const name = generateRandomWord(6);
+        const attractionId = hoaxer.datatype.number({ min: 1, max: 499 });
+        const description = hoaxer.lorem.text();
+        const additionnalDatas={};
+        const images = generateRandomImageLinks(5);
+        services.push({
+            id: id,
+            name: name,
+            attractionId:attractionId,
+            additionnalDatas:additionnalDatas,
+            price: hoaxer.commerce.price(),
+            description:description,
+            images:images
+        })
+
+    }
+    return services
+}
+
+module.exports = generateServices()
+
--- mock/attractions/locations.js
+++ mock/attractions/locations.js
@@ -1,40 +1,34 @@
 // La table Locations stocke les informations sur les lieux touristiques proposés pour chaque pays,
 // en référençant la table Countries
-module.exports=[
-    {
-        id: 1,
-        name: "Valee de Bana",
-        description: "",
-        images: [],
-        adresse :{}
-    },
-    {
-        id: 2,
-        name: "Tagidor",
-        description: "",
-        images: [],
-        adresse : {}
-    },
-    {
-        id: 3,
-        name: "Parc de Waza",
-        description: "",
-        images: [],
-        adresse : {}
-    },
-    {
-        id: 4,
-        name: "Chute de Fongo-tongo",
-        description: "",
-        images: [],
-        adresse : {}
-    },
-    {
-        id: 5,
-        name: "Grand Mall",
-        description: "",
-        images: [],
-        adresse : {}
-    },
-]
+const { generateRandomWord, generateRandomImageLinks, getRandomSubarray } = require('../globalFunctions');
+const categories = require("../categories/categories");
+const addresses = require("../adresses/adresses");
+const hoaxer = require('hoaxer');
+
+function generateLocations () {
+    const rates =[0,0.5,1,1.5,2,2.5,3,3.5,4,4.5,5];
+    const randomCategories = getRandomSubarray(categories, 4);
+    const locations = []
+    for (let id = 1; id <= 500; id += 1) {
+
+        const name = generateRandomWord(6);
+        const rate = hoaxer.random.arrayElement(rates);
+        const addressId = hoaxer.random.arrayElement(addresses).id;
+        const description = hoaxer.lorem.text();
+        const images = generateRandomImageLinks(5);
+        locations.push({
+            id: id,
+            name: name,
+            rate:rate,
+            categories:randomCategories,
+            adresseId: addressId,
+            description:description,
+            images:images
+        })
+
+    }
+    return locations
+}
+
+module.exports = generateLocations()
 
